4b478c3f5ed02a3f7045cb9e4141d1f2eb146b2f
Arav Agarwal <68903495+Arav-Agarwal18@users.noreply.github.com>
2026-04-01 16:45:51 -0500

    Deleted migration.sql

--- apps/platform/prisma/migrations/20260326212140_test/migration.sql
+++
@@ -1,9 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."Entry" ADD COLUMN     "totalPositionsGained" INTEGER NOT NULL DEFAULT 0;
-
--- AlterTable
-ALTER TABLE "public"."RawResultUpload" ADD COLUMN     "sessionLabel" TEXT;
-
--- AlterTable
-ALTER TABLE "public"."Venue" ADD COLUMN     "appleMapsUrl" TEXT,
-ADD COLUMN     "googleMapsUrl" TEXT;
